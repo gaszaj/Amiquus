ae368febdeae5ab69217c27f6f31267d3bcbd61b
Nandino Zderic <nixo.zderic@gmail.com>
2025-05-16 17:43:27 +0200

    Compression added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,24 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    (await import("@playform/compress")).default({
+      // Enable all compression types
+      CSS: true,
+      HTML: true,
+      Image: true,
+      JavaScript: true,
+      JSON: true,
+      SVG: true,
+      // Set logging level (0 = none, 1 = errors, 2 = all)
+      Logger: 2,
+      // Compress the default output directory
+      Path: ["./dist"]
+    })
+  ],
   site: 'https://eusignal.com',
-    image: {
+  image: {
     service: passthroughImageService(),
   },
 });
